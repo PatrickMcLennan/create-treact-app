400a913e6507dfcf3e3db1c76127c65432c6d629
Patrick McLennan <patrick.a.mclennan@gmail.com>
2020-09-12 00:08:35 -0400

    state testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 
-import { fireEvent, render } from "@testing-library/react";
+import { cleanup, fireEvent, render } from "@testing-library/react";
 import { App } from "./App";
 import "@testing-library/jest-dom";
+import { act } from "react-dom/test-utils";
 
-test(`<App />`, () => {
-  const { queryByTestId } = render(<App />);
-  const [h1, button] = [queryByTestId(`h1`), queryByTestId(`button`)];
+const { queryByTestId } = render(<App />);
+const [h1, button] = [queryByTestId(`h1`), queryByTestId(`button`)];
+
+afterEach(cleanup);
+
+test(`h1`, () => {
   expect(h1.textContent).toBe(`Hello World!`);
+});
+
+test(`button`, async () => {
   expect(button.textContent).toBe(`0`);
-  fireEvent.click(button);
+  await act(async () => {
+    fireEvent.click(button);
+  });
   expect(button.textContent).toBe(`1`);
 });
